828cfe38575805c59a97e4744b7abcb9d29270f5
mkama <kamalimaduddin@gmail.com>
2024-11-06 15:58:19 +0700

    minor env fix

--- utils/db.js
+++ utils/db.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('sqlz_contact', 'root', 'root', {
-  host: 'localhost',
+const sequelize = new Sequelize(process.env.DATABASE, process.env.USER, process.env.PASSWORD, {
+  host: process.env.HOST,
   dialect: 'mysql',
 });
 
